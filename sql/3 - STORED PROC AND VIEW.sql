52b2097ea8e2b736076deffda7189ae229dcb6d9
dwinpls <moratodwayne@yahoo.com>
2020-06-03 01:14:45 +0800

    Update 3 - STORED PROC AND VIEW.sql

--- sql/3 - STORED PROC AND VIEW.sql
+++ sql/3 - STORED PROC AND VIEW.sql
@@ -1,19 +1,35 @@
-CREATE VIEW view_gender AS SELECT * FROM form_gender;
-CREATE VIEW view_civilStatus AS SELECT * FROM form_civilStatus;
-CREATE VIEW view_religion AS SELECT * FROM form_religion;
-CREATE VIEW view_clientAssigned AS SELECT * FROM form_clientAssigned;
-CREATE VIEW view_examStatus AS SELECT * FROM form_examStatus;
-CREATE VIEW view_educAttain AS SELECT * FROM form_educationAttain;
-CREATE VIEW view_sourcedBy AS SELECT * FROM form_sourcedBy;
-CREATE VIEW view_applicantStatus AS SELECT * FROM form_applicantStatus;
-CREATE VIEW view_membershipType AS SELECT * FROM form_membershipType;
-CREATE VIEW view_violation AS SELECT * FROM form_violation;
-CREATE VIEW view_offense AS SELECT * FROM form_offense;
-CREATE VIEW view_claimType AS SELECT * FROM form_claimType;
-CREATE VIEW view_deathClaim AS SELECT * FROM form_deathClaim;
-CREATE VIEW view_loanType AS SELECT * FROM form_loanType;
-CREATE VIEW view_laboratory AS SELECT * FROM form_laboratory;
-CREATE VIEW view_addressState AS SELECT * FROM form_addressState;
+CREATE VIEW view_gender AS SELECT * FROM form_gender
+ GO
+CREATE VIEW view_civilStatus AS SELECT * FROM form_civilStatus
+ GO
+CREATE VIEW view_religion AS SELECT * FROM form_religion
+ GO
+CREATE VIEW view_clientAssigned AS SELECT * FROM form_clientAssigned
+ GO
+CREATE VIEW view_examStatus AS SELECT * FROM form_examStatus
+ GO
+CREATE VIEW view_educAttain AS SELECT * FROM form_educationAttain
+ GO
+CREATE VIEW view_sourcedBy AS SELECT * FROM form_sourcedBy
+ GO
+CREATE VIEW view_applicantStatus AS SELECT * FROM form_applicantStatus
+ GO
+CREATE VIEW view_membershipType AS SELECT * FROM form_membershipType
+ GO
+CREATE VIEW view_violation AS SELECT * FROM form_violation
+ GO
+CREATE VIEW view_offense AS SELECT * FROM form_offense
+ GO
+CREATE VIEW view_claimType AS SELECT * FROM form_claimType
+ GO
+CREATE VIEW view_deathClaim AS SELECT * FROM form_deathClaim
+ GO
+CREATE VIEW view_loanType AS SELECT * FROM form_loanType
+ GO
+CREATE VIEW view_laboratory AS SELECT * FROM form_laboratory
+ GO
+CREATE VIEW view_addressState AS SELECT * FROM form_addressState
+ GO
 
 CREATE PROCEDURE employee_insert
     (@EmployeeID int, @Name Varchar(30), @Age int, @Mobile int)
